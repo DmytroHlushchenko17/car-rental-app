1a875835d290a301aade2e74f9351dd864d2217f
Dmytro Hlushchenko <dimalifecell2019@gmail.com>
2026-02-07 18:18:37 +0200

    add env fail

--- lib/carsService.ts
+++ lib/carsService.ts
@@ -1,14 +1,15 @@
 import { Car, GetCarsResponse } from "@/types/cars";
 import axios from "axios";
+const api = process.env.KEY_BASE_URL;
 
 export async function getAllCars(page: number = 1, limit: number = 12) {
-  const url = `https://car-rental-api.goit.global/cars?page=${page}&limit=${limit}`;
+  const url = `${api}/cars?page=${page}&limit=${limit}`;
   const res = await axios.get<GetCarsResponse>(url);
   return res.data;
 }
 
 export async function getCarDetails(id: string) {
-  const url = `https://car-rental-api.goit.global/cars/${id}`;
+  const url = `${api}/cars/${id}`;
   const res = await axios.get<Car>(url);
   return res.data;
 }
